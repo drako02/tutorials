00916988c3048f70b238d45989b0d5352ec2158c
drako02 <andrewappah.aa@gmail.com>
2025-06-05 10:12:48 +0000

    [UPDATE] estate module: reorder data entries in manifest for improved structure

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,8 +6,8 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
-        "views/actions.xml",
         "views/estate_property_views.xml",
+        "views/actions.xml",
         "views/estate_menus.xml",
     ],
     "application": True,
